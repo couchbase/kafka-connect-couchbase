30f010fa74febbce8c55a37a4e0a9b591f49f767
David Nault <david.nault@couchbase.com>
2021-08-16 15:55:34 -0700

    DOC-8257 Clarify Persistence Polling Interval docs
    Change-Id: I14dcb42657b6bfe2d8516698bef4cb1269b3e5ba
    Reviewed-on: http://review.couchbase.org/c/kafka-connect-couchbase/+/159458
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Richard Smedley <richard.smedley@couchbase.com>
    Reviewed-by: David Nault <david.nault@couchbase.com>

--- src/main/java/com/couchbase/connect/kafka/config/source/DcpConfig.java
+++ src/main/java/com/couchbase/connect/kafka/config/source/DcpConfig.java
@@ -35,9 +35,27 @@ public interface DcpConfig {
   CompressionMode compression();
 
   /**
-   * How frequently to poll Couchbase Server to see which changes are ready
-   * to be published. Specify `0` to disable polling, or an integer followed
-   * by a time qualifier (example: 100ms)
+   * When a Couchbase Server node fails over, documents on the failing node
+   * that haven't been fully replicated may be "rolled back" to a previous state.
+   * To ensure consistency between Couchbase and the Kafka topic, the connector
+   * can defer publishing a document to Kafka until it has been saved to disk
+   * on all replicas.
+   * <p>
+   * To enable this feature, specify a non-zero persistence polling interval.
+   * The interval is how frequently the connector asks each Couchbase node
+   * which changes have been fully replicated and persisted. This ensures
+   * consistency between Couchbase and Kafka, at the cost of additional latency
+   * and bandwidth usage.
+   * <p>
+   * To disable this feature, specify a zero duration (`0`). In this mode the
+   * connector publishes changes to Kafka immediately, without waiting for
+   * replication. This is fast and uses less network bandwidth, but can result
+   * in publishing "phantom changes" that don’t reflect the actual state of
+   * a document in Couchbase after a failover.
+   * <p>
+   * CAUTION: When connecting to an ephemeral bucket, always disable
+   * persistence polling by setting this config option to `0`,
+   * otherwise the connector will never publish any changes.
    */
   @Default("100ms")
   Duration persistencePollingInterval();
