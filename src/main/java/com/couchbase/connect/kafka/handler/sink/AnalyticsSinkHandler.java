d81d8c49eea8990a3af9bcd41bd0f8a3bb441383
shahrzadshirazi <shahrzad.shirazi@couchbase.com>
2022-08-23 18:38:56 -0700

    KAFKAC-294 AnalyticsSinkHandler
    AnalyticsSinkHandler directly upserts/deletes the documents in/from the Analytics service.
    
    Change-Id: I08bc7beab71d90eddb08e141bcc80ee43522621d
    Reviewed-on: https://review.couchbase.org/c/kafka-connect-couchbase/+/179133
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: David Nault <david.nault@couchbase.com>

---
+++ src/main/java/com/couchbase/connect/kafka/handler/sink/AnalyticsSinkHandler.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2021 Couchbase, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.couchbase.connect.kafka.handler.sink;
+
+import com.couchbase.client.core.annotation.Stability;
+import com.couchbase.client.java.analytics.ReactiveAnalyticsResult;
+import com.couchbase.client.java.json.JsonObject;
+import com.couchbase.connect.kafka.config.sink.CouchbaseSinkConfig;
+import com.couchbase.connect.kafka.util.config.ConfigHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
+import org.apache.kafka.common.config.ConfigException;
+import static com.couchbase.client.java.analytics.AnalyticsOptions.analyticsOptions;
+
+
+/**
+ * This class is used when we have a source and Analytics service as the sink.
+ * We can extend this class to deal with different sources.
+ */
+@Stability.Volatile
+public class AnalyticsSinkHandler implements SinkHandler {
+  private static final Logger log = LoggerFactory.getLogger(AnalyticsSinkHandler.class);
+  protected String bucketName;
+
+  @Override
+  public void init(SinkHandlerContext context) {
+  CouchbaseSinkConfig config = ConfigHelper.parse(CouchbaseSinkConfig.class, context.configProperties());
+  this.bucketName = config.bucket();
+  }
+
+  @Override
+  public SinkAction handle(SinkHandlerParams params) {
+    String documentId = getDocumentId(params);
+    SinkDocument doc = params.document().orElse(null);
+    String keySpace = keyspace(bucketName,params.collection().scopeName(), params.collection().name());
+    if (doc != null) {
+      final JsonObject node;
+      try {
+        node = JsonObject.fromJson(doc.content());
+      } catch (Exception e) {
+        log.warn("could not generate analytics statement from node (not json)", e);
+        return SinkAction.ignore();
+      }
+
+      if (node.isEmpty()) {
+        log.warn("could not generate analytics statement from empty node");
+        return SinkAction.ignore();
+      }
+
+      String statement = upsertStatement(keySpace, node);
+
+      Mono<?> action = Mono.defer(() ->
+              params.cluster()
+                  .analyticsQuery(statement, analyticsOptions().parameters(node))
+                  .map(ReactiveAnalyticsResult::metaData)); // metadata arrival signals query completion
+
+      ConcurrencyHint concurrencyHint = ConcurrencyHint.of(documentId);
+      return new SinkAction(action, concurrencyHint);
+    } else {
+      // when doc is null we are deleting the document
+      String statement = deleteStatement(keySpace, documentId);
+      Mono<?> action = Mono.defer(() ->
+              params.cluster()
+                      .analyticsQuery(statement)
+                      .map(ReactiveAnalyticsResult::metaData)); // metadata arrival signals query completion
+
+      ConcurrencyHint concurrencyHint = ConcurrencyHint.of(documentId);
+      return new SinkAction(action, concurrencyHint);
+
+    }
+  }
+
+  private String upsertStatement(String keySpace, JsonObject values) {
+    return "UPSERT INTO " + keySpace + " ([" + values + "]);";
+  }
+
+  private String deleteStatement(String keySpace, String documentId) {
+    return "DELETE FROM " + keySpace + " WHERE _id= \"" + documentId + "\" ;";
+  }
+
+  @Override
+  public String toString() {
+    return "AnalyticsSinkHandler{" + ", bucketName='" + bucketName + '\'' + '}';
+  }
+
+  protected static String keyspace(String bucketName, String scope, String collection) {
+    if(scope.equals("") || collection.equals("")){
+      throw new ConfigException("Missing required configuration for scope and collection.");
+    }
+    return  "`" + bucketName + "`.`" + scope + "`.`" + collection + "`";
+  }
+}
