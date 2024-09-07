ae1be42b82e440d7ace0990fd30a848b4d6fc815
Nathan Ekstrom <nathan.ekstrom@netdocuments.com>
2024-09-07 00:04:43 -0600

    Changed java version to 11. Finished transform that pulls out specific fields from a json object.

--- src/main/java/com/netdocuments/connect/kafka/transforms/JsonFieldsExtractor.java
+++ src/main/java/com/netdocuments/connect/kafka/transforms/JsonFieldsExtractor.java
@@ -1,12 +1,16 @@
 package com.netdocuments.connect.kafka.transforms;
 
+import org.apache.kafka.common.config.AbstractConfig;
 import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.common.utils.ByteBufferInputStream;
 import org.apache.kafka.connect.connector.ConnectRecord;
 import org.apache.kafka.connect.errors.DataException;
 import org.apache.kafka.connect.transforms.Transformation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.couchbase.connect.kafka.util.JsonPropertyExtractor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -14,6 +18,8 @@ import java.nio.ByteBuffer;
 import java.util.Map;
 
 public class JsonFieldsExtractor<R extends ConnectRecord<R>> implements Transformation<R> {
+
+  private static final Logger log = LoggerFactory.getLogger(JsonFieldsExtractor.class);
 
   public static final String OVERVIEW_DOC = "Extracts specified fields from a JSON message";
 
@@ -23,38 +29,51 @@ public class JsonFieldsExtractor<R extends ConnectRecord<R>> implements Transfor
           "Comma-separated list of JSON fields to extract");
 
   private String[] fields;
+  private final ObjectMapper objectMapper = new ObjectMapper();
 
   @Override
   public void configure(Map<String, ?> props) {
-    // final SimpleConfig config = new SimpleConfig(CONFIG_DEF, props);
-    // fields = config.getString(FIELDS_CONFIG).split(",");
-    fields = new String[] { "envProps.acl", "documents.1.docProps.id", "envProps.url" };
+    final AbstractConfig config = new AbstractConfig(CONFIG_DEF, props);
+    fields = config.getString(FIELDS_CONFIG).split(",");
+    // fields = new String[] { "envProps.acl", "documents.1.docProps.id",
+    // "envProps.url" };
   }
 
   @Override
   public R apply(R record) {
     final Object value = record.value();
-    final Map<String, Object> newValue;
+    final Map newValue;
     try {
       if (value == null) {
         return record;
-
       } else if (value instanceof byte[]) {
         newValue = JsonPropertyExtractor.extract(new ByteArrayInputStream((byte[]) value), fields);
+        log.info("Extracted fields: {}", newValue);
       } else if (value instanceof ByteBuffer) {
         try (ByteBufferInputStream in = new ByteBufferInputStream((ByteBuffer) value)) {
           newValue = JsonPropertyExtractor.extract(in, fields);
+          log.info("Extracted fields: {}", newValue);
         }
-
       } else {
         throw new DataException(getClass().getSimpleName()
             + " transform expected value to be a byte array or ByteBuffer but got " + value.getClass().getName());
       }
 
-      return record.newRecord(record.topic(), record.kafkaPartition(),
+      byte[] newValueBytes;
+      if (newValue.isEmpty()) {
+        return null;
+      } else {
+        newValueBytes = objectMapper.writeValueAsBytes(newValue);
+      }
+
+      R newRecord = record.newRecord(record.topic(), record.kafkaPartition(),
           record.keySchema(), record.key(),
-          null, newValue,
+          record.valueSchema(), newValueBytes,
           record.timestamp());
+
+      // log.info("New Record: {}", newRecord);
+
+      return newRecord;
 
     } catch (IOException e) {
       throw new DataException(
